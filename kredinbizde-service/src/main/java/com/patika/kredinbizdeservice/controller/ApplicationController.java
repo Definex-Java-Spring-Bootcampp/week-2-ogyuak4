4f9c27bb680316b4ba95d0c7c29a7bbdc4ca64b3
ogulcan <ogyuak4@gmail.com>
2024-03-24 05:25:00 +0300

    son olarak Kredi başvurularını kaydeden end-point

--- kredinbizde-service/src/main/java/com/patika/kredinbizdeservice/controller/ApplicationController.java
+++ kredinbizde-service/src/main/java/com/patika/kredinbizdeservice/controller/ApplicationController.java
@@ -3,10 +3,9 @@ package com.patika.kredinbizdeservice.controller;
 import com.patika.kredinbizdeservice.model.Application;
 import com.patika.kredinbizdeservice.service.ApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +23,11 @@ public class ApplicationController {
     @GetMapping("/user/{email}")
     public List<Application> getApplicationsByEmail(@PathVariable String email) {
         return applicationService.getApplicationsByEmail(email);
+    }
+
+    @PostMapping("/loan")
+    public ResponseEntity<String> applyForLoan(@RequestBody Application application) {
+        applicationService.applyForLoan(application);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Loan application başarıyla kaydedildi.");
     }
 }
--- kredinbizde-service/src/main/java/com/patika/kredinbizdeservice/service/ApplicationService.java
+++ kredinbizde-service/src/main/java/com/patika/kredinbizdeservice/service/ApplicationService.java
@@ -3,6 +3,8 @@ package com.patika.kredinbizdeservice.service;
 
 import com.patika.kredinbizdeservice.model.Application;
 import com.patika.kredinbizdeservice.model.User;
+import com.patika.kredinbizdeservice.repository.ApplicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -13,7 +15,7 @@ import java.util.stream.Collectors;
 @Service
 public class ApplicationService {
 
-    // Mocked data
+
     private List<Application> applications = new ArrayList<>();
 
     public List<Application> getApplicationsByEmail(String email) {
@@ -24,5 +26,15 @@ public class ApplicationService {
 
     public void addApplication(Application application) {
         applications.add(application);
+    }
+    private final ApplicationRepository applicationRepository;
+
+    @Autowired
+    public ApplicationService(ApplicationRepository applicationRepository) {
+        this.applicationRepository = applicationRepository;
+    }
+
+    public void applyForLoan(Application application) {
+        applicationRepository.save(application);
     }
 }
